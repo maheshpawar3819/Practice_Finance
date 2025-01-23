e5d9e598fb210b469f2a327e6baffe14f8f0c625
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-01-23 15:25:46 +0530

    add linked in auth package and add srategyes

--- Atuh_App/Backend/passport.js
+++ Atuh_App/Backend/passport.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const linkedinStrategy = require("passport-linkedin-oauth2").Strategy;
 const passport = require("passport");
 
+//google strategy
 passport.use(
   new GoogleStrategy(
     {
@@ -8,6 +10,22 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
       scope: ["profile", "email"],
+    },
+    function (accessToken, refreshToken, profile, callback) {
+      callback(null, profile);
+    }
+  )
+);
+
+//linkedin strategy
+
+passport.use(
+  new linkedinStrategy(
+    {
+      clientID: process.env.LINKEDIN_CLIENT_ID,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+      callbackURL: "/auth/linkedin/callback",
+      scope: ["r_liteprofile", "r_emailaddress"],
     },
     function (accessToken, refreshToken, profile, callback) {
       callback(null, profile);
