dfb8942bc62873f51a857483b9ef90c0591fb765
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-01-23 13:19:31 +0530

    add code for google auth

--- Atuh_App/Backend/server.js
+++ Atuh_App/Backend/server.js
@@ -1,14 +1,35 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const authRoutes = require("./Routs/authRouts");
-
+const cors = require("cors");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const passportSetup = require("./passport");
+const authRouts = require("./Routes/auth");
 const app = express();
-app.use(bodyParser.json());
 
-app.use("/auth", authRoutes);
+app.use(
+  cookieSession({
+    name: "session",
+    keys: [process.env.COOKIE_KEY],
+  })
+);
 
-const Port = process.env.PORT || 5000;
-app.listen(Port, () => {
-  console.log(`server is listning on port : ${Port}`);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,PATCH,DELETE",
+    credentials: true,
+  })
+);
+
+//router
+app.use("/auth", authRouts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is listning on port : ${port}`);
 });
