8f0b9f375f1a2fd3c2c781d71e43b50ab4a9810b
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-01-27 13:25:22 +0530

    add more params

--- Api_Fetching_2(params)/Backend/Routes/routes.js
+++ Api_Fetching_2(params)/Backend/Routes/routes.js
@@ -24,7 +24,7 @@ router.route("/companies").get(async (req, res) => {
 router.route("/companies/sector/:sector").get(async (req, res) => {
   try {
     const { sector } = req.params;
-    console.log(sector);
+    // console.log(sector);
 
     if (!sector) {
       return res.status(400).json({
@@ -50,5 +50,53 @@ router.route("/companies/sector/:sector").get(async (req, res) => {
     });
   }
 });
+
+router
+  .route("/companies/market-cap/:comparison/:value")
+  .get(async (req, res) => {
+    try {
+      const { comparison, value } = req.params;
+
+      // console.log(comparison, value);
+      if ((!comparison, !value)) {
+        return res.status(400).json({
+          error: true,
+          message: "Sector parameter is required",
+        });
+      }
+
+      let query;
+      switch (comparison) {
+        case "gt":
+          query = `SELECT * FROM companies WHERE market_cap > ?`;
+          break;
+        case "lt":
+          query = `SELECT * FROM companies WHERE market_cap < ?`;
+          break;
+        case "eq":
+          query = `SELECT * FROM companies WHERE market_cap = ? `;
+          break;
+        default:
+          return res.status(400).json({
+            error: true,
+            message:
+              "Invalid comparison parameter. Use 'gt' for greater than, 'lt' for less than, or 'eq' for equal to.",
+          });
+      }
+
+      const [result] = await db.query(query, [value]);
+
+      return res.status(200).json({
+        success: true,
+        message: `Successfully fetched companies with market cap ${comparison} ${value}`,
+        result
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Server side error",
+      });
+    }
+  });
 
 module.exports = router;
