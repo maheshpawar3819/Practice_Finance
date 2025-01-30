3a15c6860230a8118bf1b9dc827732e6f4b4d1b7
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-01-30 17:22:27 +0530

    add more code

--- Api Testing/Backend/fetchRapidApi.js
+++ Api Testing/Backend/fetchRapidApi.js
@@ -1,2 +1,19 @@
+require("dotenv").config();
 const axios = require("axios");
 
+const fetchDataFromApi = async () => {
+  try {
+    const apiKey = process.env.API_KEY;
+    const symbol = "RELIANCE.BSE";
+    const response = await axios.get(
+      `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${apiKey}
+`
+    );
+
+    console.log(response?.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchDataFromApi();
